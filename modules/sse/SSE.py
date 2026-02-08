048bbb0e2f3e0bdef52609b0691dfb8dcf5a2fe7
Hector <hesitorremitos@gmail.com>
2026-02-07 23:21:10 -0400

    refactor: Simplify SSE stream handling and improve buffer initialization

--- modules/sse/SSE.py
+++ modules/sse/SSE.py
@@ -9,25 +9,31 @@ class _SSEStream:
         return self
 
     async def __anext__(self):
-        while True:
-            await self._sse._event.wait()
-            self._sse._event.clear()
-            data = self._sse._buffer
-            if data is not None:
-                return data
+        await self._sse._event.wait()
+        self._sse._event.clear()
+        return self._sse._buffer
 
 
 class SSE:
     def __init__(self):
         self._event = asyncio.Event()
-        self._buffer = None
+        self._buffer = b""
 
-    async def send(self, data):
+    async def send(self, data, event_id=None):
         if isinstance(data, str):
             data = data.encode()
         elif not isinstance(data, bytes):
             data = str(data).encode()
-        self._buffer = b"data: " + data + b"\n\n"
+
+        if event_id is None:
+            self._buffer = b"data: " + data + b"\n\n"
+        else:
+            if not isinstance(event_id, (str, bytes)):
+                event_id = str(event_id)
+            if isinstance(event_id, str):
+                event_id = event_id.encode()
+            self._buffer = b"id: " + event_id + b"\ndata: " + data + b"\n\n"
+
         self._event.set()
 
     def stream(self):
